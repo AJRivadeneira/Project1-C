7bebad9de31a6a69e3b38e3d6a7b730d966da9df
AJRivadeneira <36230536+AJRivadeneira@users.noreply.github.com>
2018-08-23 11:27:04 -0300

    Add files via upload

---
+++ Projecto1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int unaVariable = 5;
+    float otraVariable = 4.39;
+    char oOtraVariable = 'a';
+
+    printf("El numero es: %d,\t\a y el otro numero es: %.2f\n", unaVariable, otraVariable);
+    printf("El caracter es: %c", oOtraVariable);
+
+
+    return 0;
+}
